c44b81355500f6004874862fea797999d9f5e8b7
Lucas dos Santos Camilo <lucasdossantosnet@gmail.com>
2025-04-17 15:33:02 -0300

    Estudos sobre herança com Java

--- exemplo-pacotes/src/ComputadorPedro.java
+++ exemplo-pacotes/src/ComputadorPedro.java
@@ -1,8 +1,19 @@
 public class ComputadorPedro {
     public static void main(String[] args) {
         //abrindo MSN Messenger
+        System.out.println("-------MSN-------");
         MSNMessenger msn = new MSNMessenger();
         msn.enviarMensagem();
         msn.receberMensagem();
+        //abrindo Facebook Messenger
+        System.out.println("-------Facebook-------");
+        FacebookMessenger fbm = new FacebookMessenger();
+        fbm.enviarMensagem();
+        fbm.receberMensagem();
+        //abrindo Telegram
+        System.out.println("-------Telegram-------");
+        Telegram tlg = new Telegram();
+        tlg.enviarMensagem();
+        tlg.receberMensagem();
     }
 }
